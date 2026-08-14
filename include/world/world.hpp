3ec3e24239a35b41bbc35ce4673d14334adf4b01
Alexander Farrell <info@alexanderfarrell.com>
2026-08-14 15:05:50 +0100

    Added a typemap data structure.

--- include/world/world.hpp
+++ include/world/world.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <functional>
 #include <memory>
+#include <optional>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <utility>
 #include <vector>
 #include "../util/data/slotmap.hpp"
 
@@ -10,8 +16,27 @@ class Component;
 
 class Entity {
 private:
-	std::vector<std::unique_ptr<Component>> _components;
+	std::vector<std::pair<std::type_index, std::unique_ptr<Component>>> _components;
 public:
+	template <class T> std::optional<std::reference_wrapper<T>> get() {
+		static_assert(std::is_base_of<Component, T>::value,
+		              "You can only retrieve an object of subclass Component "
+		              "from an Entity");
+		for (auto& component : this->_components) {
+			if (component.first == std::type_index(typeid(T))) {
+				return std::make_optional(std::ref(component.second));
+			}
+		}
+		return std::nullopt;
+	}
+
+	template <class T> void set(T component) {
+		static_assert(
+		    std::is_base_of<Component, T>::value,
+		    "You can only add objects of type Component to the Entity");
+		
+	}
+
 	
 };
 
